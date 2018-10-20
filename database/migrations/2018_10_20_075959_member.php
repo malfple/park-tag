c5909c2624c93902423c2549c8e9e9c445fdfac4
christopher samuel <noogai886@yahoo.com>
2018-10-20 15:08:31 +0700

    add database member

--- database/migrations/2018_10_20_075959_member.php
+++ database/migrations/2018_10_20_075959_member.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class Member extends Migration
 {
     /**
      * Run the migrations.
@@ -13,11 +13,20 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('members', function (Blueprint $table) {
             $table->increments('id');
+            
             $table->string('name');
+            $table->string('plate_no');
+            $table->string('phone_no');
             $table->string('email')->unique();
+            $table->string('no_stnk');
+
+            $table->string('username');
             $table->string('password');
+            $table->string('type_member');
+            $table->integer('balance');
+            $table->string('type_ticket');
             $table->rememberToken();
             $table->timestamps();
         });
@@ -30,6 +39,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('members');
     }
 }
