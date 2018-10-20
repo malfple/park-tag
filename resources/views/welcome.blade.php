f1a477de061a1a30838182f43787bed67219aa7e
christopher samuel <noogai886@yahoo.com>
2018-10-20 14:46:03 +0700

    lupa

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Park Tag :-)
                 </div>
 
                 <div class="links">
